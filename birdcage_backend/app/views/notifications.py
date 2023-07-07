f4f9cb98eda549e9958a8303a4782d561503cb06
Jon Bloom <git@jon.tw>
2023-07-07 13:33:11 -0400

    use ORM for notifications

--- birdcage_backend/app/views/notifications.py
+++ birdcage_backend/app/views/notifications.py
@@ -1,26 +1,17 @@
 from flask import Blueprint, request, jsonify
-import sqlite3
-from config import DATABASE_FILE
+from app.models.notifications import NotificationService, NotificationAssignment
 from app.decorators import admin_required
 
 notifications_blueprint = Blueprint('notifications', __name__)
 
 
-def get_db_connection():
-    return sqlite3.connect(DATABASE_FILE)
 
 
 # Get all of the notification services and URLs from notification_services
 @notifications_blueprint.route('/services', methods=['GET'])
 def get_notification_services():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('SELECT service_name, service_url FROM notification_services')
-    services = cursor.fetchall()
-    connection.close()
-
-    response = [{'service_name': service[0], 'service_url': service[1]} for service in services]
-    return jsonify(response)
+    notification_services = NotificationService.select().dicts()
+    return jsonify(list(notification_services))
 
 
 # Set the URL for a notification service from notification_services
@@ -28,12 +19,10 @@ def get_notification_services():
 @admin_required
 def set_notification_service_url(service_name):
     service_url = request.json.get('service_url', '')
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('UPDATE notification_services SET service_url = ? WHERE service_name = ?',
-                   (service_url, service_name))
-    connection.commit()
-    connection.close()
+    service = NotificationService.get_or_none(NotificationService.service_name == service_name)
+    if service:
+        service.service_url = service_url
+        service.save()
 
     return jsonify({'result': 'success', 'message': 'Service URL updated'})
 
@@ -41,15 +30,8 @@ def set_notification_service_url(service_name):
 # Get all of the detection actions and associated notification services from notification_assignments
 @notifications_blueprint.route('/assignments', methods=['GET'])
 def get_detection_assignments():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('SELECT detectionaction, notification_service FROM notification_assignments')
-    assignments = cursor.fetchall()
-    connection.close()
-
-    response = [{'detection_action': assignment[0], 'notification_service': assignment[1]} for assignment in
-                assignments]
-    return jsonify(response)
+    assignments = NotificationAssignment.select().dicts()
+    return jsonify(list(assignments))
 
 
 # Add a specified notification service for a particular detection action in notification_assignments
@@ -62,13 +44,7 @@ def add_detection_assignment():
     if not detection_action or not notification_service:
         return jsonify({'result': 'error', 'message': 'Missing detection_action or notification_service'})
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute(
-        'INSERT OR IGNORE INTO notification_assignments (detectionaction, notification_service) VALUES (?, ?)',
-        (detection_action, notification_service))
-    connection.commit()
-    connection.close()
+    NotificationAssignment.replace(detectionaction=detection_action, notification_service=notification_service).execute()
 
     return jsonify({'result': 'success', 'message': 'Detection assignment added'})
 
@@ -83,11 +59,7 @@ def remove_detection_assignment():
     if not detection_action or not notification_service:
         return jsonify({'result': 'error', 'message': 'Missing detection_action or notification_service'})
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('DELETE FROM notification_assignments WHERE detectionaction = ? AND notification_service = ?',
-                   (detection_action, notification_service))
-    connection.commit()
-    connection.close()
+    NotificationAssignment.delete().where(NotificationAssignment.detectionaction == detection_action,
+                                            NotificationAssignment.notification_service == notification_service).execute()
 
     return jsonify({'result': 'success', 'message': 'Detection assignment removed'})
